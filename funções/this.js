ede34bb0311295a1b229c4e3c1c9b77b4f4701dc
marianamoiolicapelari <marianamoioli@gmail.com>
2022-07-09 13:16:57 -0300

    Added exercises

---
+++ funções/this.js
@@ -0,0 +1,10 @@
+function calculaIdade(anos) {
+  return `Daqui a ${anos}, ${this.nome} terá ${this.idade + anos} anos de idade.`
+}
+
+const pessoa1 = { nome: 'Maria', idade: 30 }
+const pessoa2 = { nome: 'Carla', idade: 26 }
+const animal = { nome: 'Fiona', idade: 5, raca: 'Pug' }
+
+console.log(calculaIdade.apply(pessoa1, [10]))
+//console.log(calculaIdade.call(animal, 7))
